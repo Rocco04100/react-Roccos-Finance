ad7d38d1b10d9386bda90d6ed7decf637a70a5df
Rocco <rocco.demartino25@gmail.com>
2025-10-24 14:36:22 -0400

    spelling

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,7 +2,7 @@ const Intro = () => {
   return (
     <>
       <h1 className=" md:text-5xl sm:text-4xl text-3xl text-center font-bold text-white">
-        Welcome to Rocco's Financial Litteracy Website
+        Welcome to Rocco's Financial Literacy Website
       </h1>
       <h3 className="text-lg md:text-xl text-center text-green-300 drop-shadow-md">
        Lets start by finding out where your money is going. Enter your monthly income and spending to see your savings potential.
